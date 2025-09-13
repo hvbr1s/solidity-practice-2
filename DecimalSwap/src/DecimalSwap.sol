3fc6896c49862ca15afd50dc719cd63319fd93c4
Dan Jaheny <danjaheny@mac.home>
2025-09-13 20:28:15 +0200

    decimal_swaps

--- DecimalSwap/src/DecimalSwap.sol
+++ DecimalSwap/src/DecimalSwap.sol
@@ -15,11 +15,42 @@ contract DecimalSwap {
     IERC20Metadata public immutable tokenB;
 
     constructor(address tokenA_, address tokenB_) {
+        tokenA = IERC20Metadata(tokenA_);
+        tokenB = IERC20Metadata(tokenB_);
     }
 
     function swapAtoB(uint256 amountIn) external {
+        require(tokenA.balanceOf(msg.sender) >= amountIn, "Not enough tokens for swap");
+        require(tokenA.allowance(msg.sender, address(this)) >= amountIn, "Not enough balance");
+        
+        uint amountOut = (amountIn * 10**tokenB.decimals() / 10**tokenA.decimals());
+        require(tokenB.balanceOf(address(this)) >= amountOut, "Not enough liquidity for token out");
+
+        (bool tokenPullSuccess) = tokenA.transferFrom(msg.sender, address(this), amountIn);
+        if (!tokenPullSuccess){
+            revert();
+        }
+        (bool tokenPushSuccess) = tokenB.transfer(msg.sender, amountOut);
+        if (!tokenPushSuccess){
+            revert();
+        }
+        
     }
 
     function swapBtoA(uint256 amountIn) external {
+        require(tokenB.balanceOf(msg.sender) >= amountIn, "Not enough tokens for swap");
+        require(tokenB.allowance(msg.sender, address(this)) >= amountIn, "Not enough balance");
+        
+        uint amountOut = (amountIn * 10**tokenA.decimals() / 10**tokenB.decimals());
+        require(tokenA.balanceOf(address(this)) >= amountOut, "Not enough liquidity for token out");
+
+        (bool tokenPullSuccess) = tokenB.transferFrom(msg.sender, address(this), amountIn);
+        if (!tokenPullSuccess){
+            revert();
+        }
+        (bool tokenPushSuccess) = tokenA.transfer(msg.sender, amountOut);
+        if (!tokenPushSuccess){
+            revert();
+        }
     }
 }
