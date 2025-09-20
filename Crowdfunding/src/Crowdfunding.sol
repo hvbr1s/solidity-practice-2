8ff632f98e3c2c234eeb9a2887e39047bd826226
Dan Jaheny <danjaheny@mac.home>
2025-09-20 15:31:29 +0200

    Crowdfunding

--- Crowdfunding/src/Crowdfunding.sol
+++ Crowdfunding/src/Crowdfunding.sol
@@ -10,6 +10,7 @@ contract Crowdfunding {
     address public immutable beneficiary;
     uint256 public immutable fundingGoal;
     uint256 public immutable deadline;
+    uint256 public totalContributed;
 
     mapping(address => uint256) public contributions;
 
@@ -17,8 +18,22 @@ contract Crowdfunding {
     event CancelContribution(address indexed contributor, uint256 amount);
     event Withdrawal(address indexed beneficiary, uint256 amount);
 
-    constructor(address token_, address beneficiary_, uint256 fundingGoal_, uint256 deadline_) {
+    error CannotAcceptZeroAmount();
+    error ContributionFailed();
+    error CampaignHasEnded();
+    error CampaignIsStillOngoing();
+    error ContributionExceedsFundingGoal();
 
+
+    constructor(address token_, address beneficiary_, uint256 fundingGoal_, uint256 deadline_) {
+        require(beneficiary_ != address(0), "Beneficiary address cannot be 0");
+        require(fundingGoal_ != 0, "Funding goal must be greater than 0");
+        require(token_ != address(0), "Token address cannot be 0");
+        require(block.timestamp < deadline_, "Deadline must be in the future");
+        token = IERC20(token_);
+        beneficiary = beneficiary_;
+        fundingGoal = fundingGoal_;
+        deadline = deadline_;
     }
 
     /*
@@ -26,17 +41,51 @@ contract Crowdfunding {
      * @param amount the amount of tokens to contribute.
      */
     function contribute(uint256 amount) external {
+        if (amount == 0){
+            revert CannotAcceptZeroAmount();
+        }
+        require(block.timestamp < deadline, "Contribution period over");
+        if ((totalContributed + amount) > fundingGoal){
+            revert ContributionExceedsFundingGoal();
+        }        
+        else {
+            bool k = token.transferFrom(msg.sender, address(this), amount);
+            if (k){
+                totalContributed += amount;
+                contributions[msg.sender] += amount;
+                emit Contribution(msg.sender, amount);
+            } else{
+                revert ContributionFailed();
+            }
+        }
     }
 
     /*
      * @notice a contribution can be cancelled if the goal is not reached. Returns the tokens to the contributor.
      */ 
     function cancelContribution() external {
+        require(totalContributed < fundingGoal, "Cannot cancel after goal reached");
+        uint amountToRefund = contributions[msg.sender];
+        require(amountToRefund > 0, "You have not contributed to this campaign");
+        contributions[msg.sender] = 0;
+        totalContributed -= amountToRefund;
+        IERC20(token).transfer(msg.sender, amountToRefund);
+        emit CancelContribution(msg.sender, amountToRefund);
     }
 
     /*
      * @notice the beneficiary can withdraw the funds if the goal is reached.
      */
-    function withdraw() external {
+    function withdraw() external {  
+        require(msg.sender == beneficiary, "Only beneficiary can withdraw");
+        require(block.timestamp > deadline, "Funding period not over");
+        require(totalContributed >= fundingGoal, "Funding goal not reached");
+        uint amountToWithdraw = token.balanceOf(address(this));
+        bool ok = token.transfer(beneficiary, amountToWithdraw);
+        if (ok){
+            emit Withdrawal(beneficiary, amountToWithdraw);
+        } else {
+            revert();
+        }
     }
 }
